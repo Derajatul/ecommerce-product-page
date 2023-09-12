5bf478bd602ff329a830ffb77c809fdeca1b4e78
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-12 08:23:12 +0000

    add tailwind theme 🔧

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,58 @@ const config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    colors: {
+      orange: 'hsl(26, 100%, 55%)',
+      paleOrange: 'hsl(25, 100%, 94%)',
+      veryDarkBlue: 'hsl(220, 13%, 13%)',
+      darkGrayishBlue: 'hsl(219, 9%, 45%)',
+      grayishBlue: 'hsl(220, 14%, 75%)',
+      lightGrayishBlue: 'hsl(223, 64%, 98%)',
+    },
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()]
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            background: "#FFFFFF", // or DEFAULT
+            foreground: "#11181C", // or 50 to 900 DEFAULT
+            primary: {
+              //... 50 to 900
+              foreground: "#FFFFFF",
+              DEFAULT: "hsl(26, 100%, 55%)",
+            },
+            // ... rest of the colors
+          },
+        },
+        dark: {
+          colors: {
+            background: "#000000", // or DEFAULT
+            foreground: "#ECEDEE", // or 50 to 900 DEFAULT
+            primary: {
+              //... 50 to 900
+              foreground: "#FFFFFF",
+              DEFAULT: "hsl(26, 100%, 55%)",
+            },
+          },
+          // ... rest of the colors
+        },
+        mytheme: {
+          // custom theme
+          extend: "dark",
+          colors: {
+            primary: {
+              DEFAULT: "#BEF264",
+              foreground: "#000000",
+            },
+            focus: "#BEF264",
+          },
+        },
+      },
+    }),
+  ],
 }
 
 export default config;
